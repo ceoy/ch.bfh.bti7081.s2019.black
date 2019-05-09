821e2a41c017a1997d3fdab8dc99d2e648f1f489
Ueli <ueli.buehler@bedag.ch>
2019-05-09 13:49:18 +0200

    project error fix

---
+++ spitexorganizer/src/main/java/ch/bfh/bti7081/s2019/black/spitexorganizer/SpitexorganizerApplication.java
@@ -0,0 +1,13 @@
+package ch.bfh.bti7081.s2019.black.spitexorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpitexorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpitexorganizerApplication.class, args);
+	}
+
+}
