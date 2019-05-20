2177e107874e4e42185753e21a129ba8e1535add
suddi <suddi@PC-MAXHEROV>
2019-05-20 08:08:45 +0200

    update evaluation ui

---
+++ spitexorganizer/src/main/java/ch/bfh/bti7081/s2019/black/spitexorganizer/evaluation/ui/EvaluationView.java
@@ -0,0 +1,16 @@
+package ch.bfh.bti7081.s2019.black.spitexorganizer.evaluation.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLayout;
+
+import ch.bfh.bti7081.s2019.black.spitexorganizer.MainLayout;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route(value = "evaluation-view", layout = MainLayout.class)
+
+public class EvaluationView extends VerticalLayout implements RouterLayout {
+
+}
---
+++ spitexorganizer/src/main/java/ch/bfh/bti7081/s2019/black/spitexorganizer/evaluation/ui/EvaluationViewCreate.java
@@ -0,0 +1,63 @@
+package ch.bfh.bti7081.s2019.black.spitexorganizer.evaluation.ui;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLayout;
+
+import ch.bfh.bti7081.s2019.black.spitexorganizer.MainLayout;
+import ch.bfh.bti7081.s2019.black.spitexorganizer.appointment.api.AppointmentApi;
+import ch.bfh.bti7081.s2019.black.spitexorganizer.appointment.ui.AppointmentDetailView;
+import ch.bfh.bti7081.s2019.black.spitexorganizer.appointment.view.dtos.AppointmentDto;
+import ch.bfh.bti7081.s2019.black.spitexorganizer.evaluation.api.EvaluationApi;
+import ch.bfh.bti7081.s2019.black.spitexorganizer.evaluation.view.dtos.EvaluationDto;
+import ch.bfh.bti7081.s2019.black.spitexorganizer.report.api.ReportApi;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Tag("evaluation-create")
+@HtmlImport("frontend://src/EvaluationCreate.html")
+@PageTitle("Create Evaluation")
+@Route(value = "evaluation-create", layout = MainLayout.class)
+public class EvaluationViewCreate  extends PolymerTemplate<AppointmentDetailView.TaskModel> implements RouterLayout, HasUrlParameter<Long> {
+	
+    private H1 patientName;
+    @Id("txt-name")
+    private Paragraph txtName;
+    @Id("txt-street")
+    private Paragraph txtStreet;
+    @Id("txt-city")
+    private Paragraph txtCity;
+    @Id("txt-phone")
+    private Paragraph txtPhone;
+    @Id("txt-mail")
+    private Paragraph txtMail;
+    
+    private EvaluationDto appointment;
+
+    private EvaluationApi appointmentApi;
+    
+	@Override
+	public void setParameter(BeforeEvent event, Long parameter) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    
+}
